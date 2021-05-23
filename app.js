b8b08434b34eaf8ef87276392bdced74adc87fff
huro3h <huro3h@gmail.com>
2021-05-23 18:44:59 +0900

    named slot

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-const MyButton = {
+// const MyButton = {
+//   template: `
+//     <button>
+//       <slot>OKです</slot>
+//     </button>
+//   `
+// }
+//
+// new Vue({
+//   el: app,
+//   components: {
+//     MyButton: MyButton
+//   }
+// })
+
+// named slot
+const MyPage = {
   template: `
-    <button>
-      <slot>OKです</slot>
-    </button>
+    <div>
+      <header>
+        <slot name="header"></slot>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
+      <footer>
+        <slot name="footer"></slot>
+      </footer>
+    </div> 
   `
-}
+} 
 
 new Vue({
   el: app,
   components: {
-    MyButton: MyButton
+    MyPage: MyPage
   }
 })

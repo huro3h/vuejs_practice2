ae399ea0e770c4911be6754628a4e060d96ca32a
huro3h <huro3h@gmail.com>
2021-06-19 12:48:16 +0900

    in unuse render function case

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-Vue.directive('fallback-image', {
-  bind: function (el, binding) {
-    console.log('bind', binding);
-    const once = binding.modifiers.once
-   
-    el.addEventListener('error', function onError() {
-      el.src = binding.value
-      if (once) {
-        el.removeEventListener('error', onError)
-      }
-    })
-  },
-  update: function (el, binding) {
-    console.log('update', binding);
-    if (binding.oldValue !== binding.value && binding.oldValue === el.src) {
-      el.src = binding.value
-    }
-  }
-})
+const MyButton = {
+  props: ['href', 'tag'],
+  template: `
+    <a v-if="(!tag && href) || tag === 'a'" :href="href || '#'">
+      <slot></slot>
+    </a>
+    <span v-else-if="tag === 'span'">
+      <slot></slot>
+    </span>
+    <button v-else>
+      <slot></slot>
+    </button>
+  `
+}
 
-const vm = new Vue({
+new Vue({
   el: '#app',
-  data: function () {
-    return {
-      altText: 'logo',
-      noImageURL: 'https://dummyimage.com/400x400/000/ffffff.png&text=no+image'
-    }
+  components: {
+    MyButton: MyButton
   }
 })

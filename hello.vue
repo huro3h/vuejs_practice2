9269ee88ca0711f4524d0d9d8453227cef307ea1
huro3h <huro3h@gmail.com>
2021-07-10 09:19:33 +0900

    hello vuejs template

---
+++ hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="message">message: {{msg}}</p>
+</template>
+
+<script>
+  export default {
+    data () {
+      return { msg: 'konnnitiwa.' }
+    }
+  }
+</script>
+
+<style>
+  .message { color: blue; }
+</style>

c3f15c5025a022acf4fd86beb58d66f39f4fe2fb
huro3h <huro3h@gmail.com>
2021-07-10 11:55:36 +0900

    css module

---
+++ index.vue
@@ -0,0 +1,16 @@
+<template>
+  <p :class="$style.message">hello!</p>
+</template>
+
+<script>
+export default {
+  name: "index",
+  created () {
+    console.log('css modules: $style', this.$style);
+  }
+}
+</script>
+
+<style module>
+.message { color: aqua; }
+</style>

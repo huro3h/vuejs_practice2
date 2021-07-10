811069d6a4a6a973c5938b64de644d06b995e5ab
huro3h <huro3h@gmail.com>
2021-07-10 18:01:20 +0900

    add task objects

--- store.js
+++ store.js
@@ -4,7 +4,20 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  // TODO
+  state: {
+    tasks: [
+      {
+        id: 1,
+        name: 'あれを買う',
+        done: false
+      },
+      {
+        id: 2,
+        name: 'これを買う',
+        done: true
+      }
+    ],
+  },
 })
 
 // ストアをエクスポート

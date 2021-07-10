11b4f6ae8999156d5cae4a48cb4548ab380597fc
huro3h <huro3h@gmail.com>
2021-07-10 11:36:49 +0900

    nested components

---
+++ root.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="root">
+    <h2 class="header">Root Component</h2>
+    <p>root..</p>
+    <foo/>
+    <bar/>
+  </div>
+</template>
+
+<script>
+import Foo from './foo'
+import Bar from './bar'
+
+export default {
+  components: {
+    Foo,
+    Bar
+  }
+}
+</script>
+
+<style>
+#root {
+  border: solid 10px orangered;
+  margin: 10px;
+  padding: 8px;
+}
+.header { font-size: 200%; }
+</style>
